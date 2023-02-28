17e1bea20c55c3a8eff7302570d4d32d718422a5
hafsa <hfsyln93@gmail.com>
2023-02-28 18:08:59 +0300

    edit

---
+++ src/util/Data.js
@@ -0,0 +1,41 @@
+const data = [
+    {
+        id: 'NaNNA463undefined',
+        name: 'Väinö Perko',
+        image: 'https://randomuser.me/api/portraits/men/13.jpg',
+        job: 'Teacher,Berlin'
+    },
+    {
+        id: '294562198',
+        name: 'Mihailo Jović',
+        image: 'https://randomuser.me/api/portraits/men/55.jpg',
+        job: 'Architect,Berlin'
+    },
+    {
+        id: 'NaNNA281undefined',
+        name: 'Nooa Harju',
+        image: 'https://randomuser.me/api/portraits/men/46.jpg',
+        job: 'Developer,Berlin'
+    },
+    {
+        id: 'ES 75 36 40 Q',
+        name: 'Ivan Medina',
+        image: 'https://randomuser.me/api/portraits/men/36.jpg',
+        job: 'Doctor,Berlin'
+
+    },
+    {
+        id: 'BT 32 99 37 P',
+        name: 'Tracey Hoffman',
+        image: 'https://randomuser.me/api/portraits/women/80.jpg',
+        job: 'Teacher,Berlin'
+
+    },
+    {
+        id: 'BB 58 36 13 U',
+        name: 'Emily Cunningham',
+        image: 'https://randomuser.me/api/portraits/women/50.jpg',
+        job: 'Influencer, Düsseldorf'
+
+    }]
+export default data;
